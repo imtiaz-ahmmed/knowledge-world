ad89002edf5ed2be52450681d66dca634d03396b
imtiaz-ahmmed <imtiazahmmed00@gmail.com>
2023-03-31 19:21:20 +0600

    bookmark containier completed

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -5,9 +5,12 @@ const Bookmark = ({ bookmarks }) => {
     return (
         <div className='bookmark-items'>
             <h2>Bookmarked Blogs: {bookmarks.length}</h2>
+
             {bookmarks.map((bookmark, index) => (
-                <h2 key={index}>{bookmark}</h2>
+                <h2 className='bookmark-item' key={index}>{bookmark}</h2>
             ))}
+
+
         </div>
     );
 };
