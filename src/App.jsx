0af7b5863ef4f4a56956feb58e8d30f402d49ddd
imtiaz-ahmmed <imtiazahmmed00@gmail.com>
2023-03-31 19:58:33 +0600

    toast added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Header from './components/Header/Header'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +13,7 @@ function App() {
       <Header></Header>
       <hr />
       <Blogs></Blogs>
-
+      <ToastContainer></ToastContainer>
     </div>
   )
 }
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,6 +3,7 @@ import Blog from '../Blog/Blog';
 import Bookmark from '../Bookmark/Bookmark';
 import ReadingTime from '../ReadingTime/ReadingTime';
 import './Blogs.css'
+import { ToastContainer, toast } from 'react-toastify';
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -36,9 +37,13 @@ const Blogs = () => {
 
     const bookmarkHandle = (title) => {
         const storedBookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+        if (storedBookmarks.includes(title)) {
+            toast("You Have Already Bookmarked This Blog");
+        }
         const updatedBookmarks = [...storedBookmarks, title];
         localStorage.setItem("bookmarks", JSON.stringify(updatedBookmarks));
         setBookmarks(updatedBookmarks);
+
     };
 
     return (
